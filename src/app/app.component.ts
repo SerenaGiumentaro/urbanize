bc75c1e2cf4006f65c32ef6d16f0038b5dc7e14d
SerenaGiumentaro <serena.giumentaro@gmail.com>
2023-04-17 19:28:12 +0200

    starting adding style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'urbanize';
 
-  hasData: boolean = false;
+  hasData: boolean = true;
 
   checkDataAfterSubmit(hasData: boolean) {
     this.hasData = hasData;
